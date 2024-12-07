2c5253a7de35eda36b13f12c9cd4d662cabf11e6
Shynitha19 <shynitha1908@gmail.com>
2024-12-07 21:40:01 +0530

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    from video_utils import read_video
+    from video_utils import save_video
+    from video_utils import detect_vehicles
+else:
+    from .video_utils import read_video
+    from .video_utils import save_video
+    from .video_utils import detect_vehicles
